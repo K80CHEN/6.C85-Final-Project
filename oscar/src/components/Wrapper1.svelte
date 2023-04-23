a3c3751ead443cb4f2254e71c4fed245c3edc88f
Audrey <ahx@mit.edu>
2023-04-23 16:46:03 -0400

    Updates with plot.

--- oscar/src/components/Wrapper1.svelte
+++ oscar/src/components/Wrapper1.svelte
@@ -1,38 +1,68 @@
 <script>
+	import { scaleLinear } from 'd3-scale';
+  import { tweened } from "svelte/motion";
+
+  let data = [
+      { foo: 4, bar: 1 },
+      { foo: 6, bar: 7 },
+      { foo: 9, bar: 5 },
+      { foo: 2, bar: 4 },
+      { foo: 8, bar: 2 },
+      { foo: 9, bar: 9 },
+      { foo: 5, bar: 3 },
+      { foo: 3, bar: 8 },
+      { foo: 1, bar: 6 },
+  ];
+
+  const tweenedX = tweened(data.map((d) => d.foo));
+  const setFoo = function () {
+      tweenedX.set(data.map((d) => d.foo));
+  };
+  const setBar = function () {
+      tweenedX.set(data.map((d) => d.bar));
+  };
+
+  let width = 400;
+  let height = 400;
+
+  let xScale = scaleLinear().domain([0, 10]).range([0, width]);
+  let yScale = scaleLinear().domain([0, 10]).range([height, 0]);
+
   export let index;
-  let container;
-  let isVisible = false;
 
   $: if (index === 0) {
-    isVisible = true;
+    setFoo();
+  } else if (index === 1 | index === 2) {
+    setBar();
   } else {
-    isVisible = false;
+    setFoo();
   }
 </script>
 
-<img
-  class="img2"
-  class:visible={isVisible}
-  bind:this={container}
-  src="http://placehold.it/300x200"
-  srcset="
-	    http://placehold.it/300x200 300w"
-  sizes="
-	    
-  70vw"
-  alt="Placeholder"
-/>
+<section>
+  <div class="chart">
+      <svg {width} {height}>
+        {#each data as d, index}
+          <circle
+            cx={xScale($tweenedX[index])}
+            cy={yScale(d.bar)}
+            r={15}
+            fill="steelblue"
+            stroke="#000000"
+          />
+        {/each}
+      </svg>
+  </div>
+</section>
 
 <style>
-  .img2 {
-    /* position: absolute; */
-    opacity: 0;
-    visibility: hidden;
-    transition: opacity 2s, visibility 2s;
-  }
-
-  .img2.visible {
-    opacity: 1;
-    visibility: visible;
-  }
+.chart {
+  background: whitesmoke;
+  width: 400px;
+  height: 400px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.2);
+  position: sticky;
+  top: 10%;
+  margin: auto;
+}
 </style>
